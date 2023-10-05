7098b0f4d49de105c6cf3d2739650ecdf8607799
noorullhasann <110535706+noorullhasann@users.noreply.github.com>
2023-10-05 15:11:34 +0530

    Add files via upload

---
+++ Back-end Day 2/repo/LoginRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.LoginModel;
+
+@Repository
+public interface LoginRepo extends JpaRepository<LoginModel, Integer>{
+	
+}
