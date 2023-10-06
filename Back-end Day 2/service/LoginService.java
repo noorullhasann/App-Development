4a252b1e7502d5ae8f6f813224e55c5b827baf35
noorullhasann <110535706+noorullhasann@users.noreply.github.com>
2023-10-06 07:24:41 +0530

    Add files via upload

---
+++ Back-end Day 2/service/LoginService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.LoginModel;
+import com.example.demo.model.SignupModel;
+import com.example.demo.repo.LoginRepo;
+@Service
+public class LoginService {
+
+	@Autowired
+	private LoginRepo loginrepo;
+	
+	public List<LoginModel> getuserdetails(){
+		return loginrepo.findAll();
+	}
+		
+	
+	public Optional<LoginModel> getuserByID(int loginid){
+		return loginrepo.findById(loginid);
+	}
+	
+	public void postDetails(LoginModel lm){
+		loginrepo.save(lm);
+	}
+	
+	public LoginModel putDetails(LoginModel lm) {
+		return loginrepo.save(lm);
+	}
+	
+	public void deletedetailsById(int loginid) {
+		loginrepo.deleteById(loginid);
+	}
+	
+	public boolean authLogin(LoginModel lm) {
+		Optional<SignupModel> userOpt = loginrepo.findByUsername(lm.getUsername());
+		
+		if (!userOpt.isPresent()) return false;
+		if (!lm.getPassword().equals(userOpt.get().getPassword())) return false;
+
+		return true;
+	}
+	
+}
